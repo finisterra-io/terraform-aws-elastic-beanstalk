c7295d4aab978560c8e0ec4c81ae809824ed06bd
Daniel J <daniel@finisterra.io>
2024-02-06 19:32:05 -0500

    Refactor elb_settings_final variable in main.tf

--- modules/enviroment/main.tf
+++ modules/enviroment/main.tf
@@ -221,7 +221,6 @@ locals {
   elb_setting_classic     = var.loadbalancer_type == "classic" ? concat(local.classic_elb_settings, local.generic_elb_settings, local.beanstalk_elb_settings) : []
 
   # If the tier is "WebServer" add the elb_settings, otherwise exclude them
-  elb_settings_final = var.tier == "WebServer" ? concat(local.elb_settings_nlb, local.elb_settings_alb, local.elb_settings_shared_alb, local.elb_setting_classic) : []
 }
 
 #
